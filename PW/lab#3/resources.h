46cfafdc243f89a0ec90d66a1e81612d219fc999
Sanda-Cotovici <sandacotovici@gmail.com>
2016-03-24 16:54:57 +0200

    added resource files

---
+++ PW/lab#3/resources.h
@@ -0,0 +1,16 @@
+#define IDB_TOOLS_GROUP 100
+#define IDB_PENCIL_TOOL 101
+#define IDB_ELLIPSE_TOOL 102
+#define IDB_RECTANGLE_TOOL 103
+#define IDB_LINE_TOOL 104
+#define IDB_ERASER_TOOL 105
+#define IDB_FILL_CHECK 106
+#define IDB_BORDER_WIDTH 107
+#define IDB_BEZIER_TOOL 109
+#define IDI_CLEAR 110
+
+///define IDs for keyboard management
+#define KEY_E	0x45  /// exit
+#define F1	    0x70  // help
+#define KEY_C   0x43  /// manage color of lisbpx
+#define RETURN	0x0D  /// output information
